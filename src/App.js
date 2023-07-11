e8b1513cdf727060cff872cc342f7df4ed184729
stephen-ics <stephenni1234@gmail.com>
2023-07-11 14:40:42 -0400

    packages + navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import './App.css';
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { Helmet } from "react-helmet";
+import { AnimatePresence } from 'framer-motion'
+
+import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
   return (
-    <div className=" text-8xl">
-      s
+    <div className=''>
+      <Helmet>
+        <title>Api-development Frontend</title>
+        <meta name="description" content="Hello! I'm Stephen! I'm a high school student at John Fraser Secondary School and an aspiring full-stack developer."></meta>
+      </Helmet>
+      <BrowserRouter>
+        <Navbar />
+        <AnimatePresence 
+        wait
+        >
+          <Routes>
+              <Route path='/' exact element={<Home/>}></Route>
+              <Route path='/about' exact element={<About/>}></Route>
+          </Routes>
+    </AnimatePresence>
+      </BrowserRouter>
     </div>
   );
 }
+
+
 
 export default App;
