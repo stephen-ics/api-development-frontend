a71b17e65302e513ef23733ee37c2f9a3c2aee8e
stephen-ics <stephenni1234@gmail.com>
2023-07-14 15:20:16 -0400

    deleting posts work

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,18 +3,46 @@ import { icons } from 'react-icons'
 import { HiHeart, HiDotsHorizontal } from "react-icons/hi";
 import { useState } from 'react'
 
-const Post = ({ user, date, title, content, pfp, image }) => {
+const Post = ({ id, user, user_id, date, title, content, pfp, image }) => {
     const [isExpanded, setIsExpanded] = useState(false);
+    const [options, setOptions] = useState(false)
+    const [data, setData] = useState([])
 
     const handleToggleExpand = () => {
         setIsExpanded(!isExpanded);
     };
 
+    const handleOptions = () => {
+        setOptions(!options)
+    }
+    
+    const deletePost = () => {
+        fetch(`https://www.api-development.xyz/posts/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('access_token')}`
+            }    
+        }).then((response) => {
+            if (response.status === 204) {
+                setTimeout(() => {
+                    window.location.reload();
+                    }, 1000);
+                }
+            }
+        )
+    }
+    
+
+    const editPost = () => {
+        setOptions(!options)
+    }
+
     const maxLength = 100
     const shouldTruncate = content.length > maxLength;
 
   return (
-    <div className='bg-white my-4 lg:px-12 px-8 pt-8 pb-4 rounded-xl border-gray-200 border-2 border-solid w-full'>
+    <div className='bg-white my-4 lg:px-12 px-8 pt-8 pb-4 rounded-xl border-gray-200 border-2 border-solid w-full relative shadow-xl'>
             <div className='flex items-center'>
                 <div className='flex w-full justify-between'>
                     <div className='flex items-center'>
@@ -25,7 +53,23 @@ const Post = ({ user, date, title, content, pfp, image }) => {
                         </div>
                     </div>
                     <div className='flex items-center'>
-                        <HiDotsHorizontal size={25}/>
+                        { options ? (
+                            <div className='absolute top-20 right-0'>
+                                <div className='flex flex-col'>
+                                    <button className='border-solid border-black border-2' onClick={deletePost}>Delete</button>
+                                    <button className='border-solid border-black border-2' onClick={editPost}>Edit</button>
+                                </div>
+                                
+                            </div>
+                        ) : (
+                            <div>
+
+                            </div>
+                        )
+                        }
+                        <button onClick={handleOptions}>
+                            <HiDotsHorizontal size={25}/>   
+                        </button>
                     </div>
                 </div>
             </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -28,7 +28,7 @@ const Home = () => {
     <div className='flex justify-center'>
       <div className='w-1/2 mx-0'>
       {data.map((post, index) => (
-        <Post key={index} user={post.Post.user.email} date={post.Post.created_at} title={post.Post.title} content={post.Post.content} pfp={Logo} image={Logo}/>
+        <Post key={index} id={post.Post.id} user={post.Post.user.email} user_id={post.Post.user_id} date={post.Post.created_at} title={post.Post.title} content={post.Post.content} pfp={Logo} image={Logo}/>
       ))}
     
       </div>
