c89292f856047128bcb4246000be597dd7bdfc36
stephen-ics <stephenni1234@gmail.com>
2023-07-21 19:42:19 -0400

    fixed landing page button

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,15 +1,22 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import HomeSection from '../components/HomeSection'
 import Logo from '../pictures/Logo.png'
 
 const Landing = () => {
+  const navigate = useNavigate()
+
+  const handleStart = () => {
+    navigate('/login')
+  }
+
   return (
     <div>
       <div className="flex flex-col items-center text-zinc-900">
         <div className='bg-black bg-opacity-20 rounded-lg lg:p-32 flex flex-col items-center'>
           <h1 className='lg:text-8xl text-6xl font-bold'>ConnectX</h1>
           <h2 className='lg:text-6xl text-4xl my-4'>X marks the Spot: ConnectX, Your Social Destination!</h2>
-          <button className='lg:text-6xl text-4xl my-6 bg-blue-400 lg:px-10 lg:py-6 px-4 py-4 rounded-lg'>Get Started!</button>
+          <button className='lg:text-6xl text-4xl my-6 bg-blue-400 lg:px-10 lg:py-6 px-4 py-4 rounded-lg' onClick={handleStart}>Get Started!</button>
         </div>
       </div>
       <div className='lg:mx-72 mb-10'>
