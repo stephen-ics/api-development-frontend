1429e129adae70fddf315d41f0f1b9ed0a3bc820
stephen-ics <stephenni1234@gmail.com>
2023-07-21 23:17:56 -0400

    added search bar

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom'
 
 const Feed = () => {
   const [data, setData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('')
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -23,7 +24,7 @@ const Feed = () => {
   }, [])
 
   useEffect(() => {
-    fetch('https://www.api-development.xyz/posts/', {
+    fetch(`https://www.api-development.xyz/posts/main?search=${encodeURIComponent(searchQuery)}`, {
       method: 'GET',
       headers: {
           'Content-Type': 'application/json',
@@ -32,12 +33,19 @@ const Feed = () => {
     }).then(response => response.json())
     .then(data => setData(data))
     
-  }, [])
+  }, [searchQuery])
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value)
+  }
 
 
   return (
     <div className='flex justify-center'>
       <div className='w-1/2 mx-0'>
+      <form className='w-full'>
+        <input type='text' className='bg-white w-full pl-8 py-5 rounded-2xl' placeholder='Search for a Post' value={searchQuery} onChange={handleSearchChange}/>
+      </form>
       {data &&
         data.map((post, index) => (
           <Post key={index} id={post.Post.id} user={post.Post.user.email} user_id={post.Post.user_id} date={post.Post.created_at} title={post.Post.title} content={post.Post.content} pfp={Logo} image={post.Post.image} />
